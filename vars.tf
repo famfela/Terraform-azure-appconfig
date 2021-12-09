e653c7379f20b2b4cd934c663bc56bbd0e26d739
hermann1990 <76397203+hermann1990@users.noreply.github.com>
2021-12-09 08:43:57 -0500

    main.tf file

--- vars.tf
+++ vars.tf
@@ -12,12 +12,6 @@ variable "sku" {
   description = "basic, free, standard, standard2, standard3, storage_optimized_l1 and storage_optimized_l2"
   type = string
   default = "free"
-
-
-variable "public_network_access_enabled" {
-  description = "Whether or not public network access is allowed for this resource."
-  type = bool
-  default = false
 }
 variable "resource_group_name" {
   description = "example-resources"
