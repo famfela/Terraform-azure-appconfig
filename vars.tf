7e82196b705bec34aa2ee6dcc43f6b75aca7d470
hermann1990 <76397203+hermann1990@users.noreply.github.com>
2021-12-09 08:40:48 -0500

    main.tf file

--- vars.tf
+++ vars.tf
@@ -12,18 +12,7 @@ variable "sku" {
   description = "basic, free, standard, standard2, standard3, storage_optimized_l1 and storage_optimized_l2"
   type = string
   default = "free"
-}
 
-variable "partition_count" {
-  description = "The number of partitions which should be created. Can only be configured when using a standard sku."
-  type = number
-  default=1
-}
-variable "replica_count" {
-  description = "The number of replicas which should be created. Can only be configured when using a standard sku"
-  type = number
-  default=1
-}
 
 variable "public_network_access_enabled" {
   description = "Whether or not public network access is allowed for this resource."
